0e003c91544410d806901b58162f75290a5c3aa0
namenice <32704475+namenice@users.noreply.github.com>
2025-07-19 15:27:20 +0700

    Delete src/notifiers/notifiers directory

--- src/notifiers/notifiers/notifier.js
+++
@@ -1,6 +0,0 @@
-class Notifier {
-  constructor(name) { this.name = name; }
-  /* eslint-disable-next-line no-unused-vars */
-  async send(payload) { throw new Error('send() not implemented'); }
-}
-module.exports = Notifier;
