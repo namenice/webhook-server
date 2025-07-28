8a2583ea2fcec1be3e0b58303b52c2d8ff36ff75
namenice <32704475+namenice@users.noreply.github.com>
2025-07-28 15:44:27 +0700

    Update oneplatform.js

--- src/notifiers/oneplatform.js
+++ src/notifiers/oneplatform.js
@@ -1,6 +1,8 @@
+// src/notifiers/oneplatform.js
+
 const axios = require('axios');
 require('dotenv').config();
-const { formatOnePlatformMessage } = require('../utils/formatters');
+const { formatOnePlatformMessage } = require('../utils/oneplatformFormatter');
 
 const CHATBOT_API_URL = process.env.CHATBOT_API_URL;
 const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
@@ -8,6 +10,7 @@ const RECIPIENT_ID = process.env.RECIPIENT_ID;
 
 async function sendToOnePlatform(payload) {
   const message = formatOnePlatformMessage(payload);
+  console.log('✅ Sent to OnePlatform', message);
 
   const requestBody = {
     to: RECIPIENT_ID,
@@ -24,14 +27,13 @@ async function sendToOnePlatform(payload) {
   try {
     const res = await axios.post(CHATBOT_API_URL, requestBody, { headers });
     console.log('✅ Sent Alert to OnePlatform');
-    //console.log('✅ Sent to OnePlatform', res.data);
   } catch (err) {
     console.error('❌ Failed to send to OnePlatform:', err.message);
-    //console.error('❌ Failed to send to OnePlatform:', err.message);
     throw err;
   }
 }
 
 module.exports = {
-  send: sendToOnePlatform
+  send: sendToOnePlatform,
+  name: 'oneplatform'
 };
