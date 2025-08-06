dc2b2571aecc3768c864c39af40f57e07894470e
namenice <32704475+namenice@users.noreply.github.com>
2025-08-06 20:25:56 +0700

    Update email.js

--- backend/src/notifiers/email.js
+++ backend/src/notifiers/email.js
@@ -3,41 +3,51 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 const { formatEmailMessage } = require('../utils/emailFormatter');
 
+const {
+    MAIL_HOST,
+    MAIL_PORT,
+    MAIL_SECURE,
+    MAIL_TO,
+    MAIL_FROM,
+    MAIL_CC
+} = process.env;
+
+if (!MAIL_HOST || !MAIL_PORT || !MAIL_FROM || !MAIL_TO) {
+    console.error('❌ Missing required environment variables for email sending. Please check your .env file.');
+    throw new Error('Missing required environment variables for email sending.');
+}
+
+const parsedMailPort = parseInt(MAIL_PORT, 10);
+const parsedMailSecure = MAIL_SECURE === 'true';
 const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: parseInt(process.env.MAIL_PORT, 10),
-  secure: process.env.MAIL_SECURE === 'true', // true for 465, false for others
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS
-  }
+    host: MAIL_HOST,
+    port: parsedMailPort,
+    secure: parsedMailSecure,
 });
 
 async function send(payload) {
-  const message = formatEmailMessage(payload);
-
-  if (!message) {
-    console.log('📭 No firing alerts. Email will not be sent.');
-    return;
-  }
-
-  const mailOptions = {
-    from: `"Alert Bot" <${process.env.MAIL_USER}>`,
-    to: process.env.MAIL_TO,
-    subject: '⚠️[Alert] Critical Infra From Openlandscape Cloud⚠️',
-    html: message
-  };
-
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('📧 Email sent:', info.messageId);
-  } catch (error) {
-    console.error('❌ Failed to send email:', error.message);
-  }
+    const message = formatEmailMessage(payload);
+    if (!message) {
+        console.log('📭 No firing alerts. Email will not be sent.');
+        return;
+    }
+    const mailOptions = {
+        from: MAIL_FROM,
+        to: MAIL_TO,
+        cc: MAIL_CC,
+        subject: '⚠️[Alert] Critical Infra From Openlandscape Cloud⚠️',
+        html: message,
+    };
+    console.log('Preparing to send mail...');
+    try {
+        const info = await transporter.sendMail(mailOptions);
+        console.log('📧 Email sent successfully! Message ID:', info.messageId);
+    } catch (error) {
+        console.error('❌ Failed to send email:', error.message);
+    }
 }
 
 module.exports = {
-  send,
-  name: 'email'
+    send,
+    name: 'email'
 };
-
